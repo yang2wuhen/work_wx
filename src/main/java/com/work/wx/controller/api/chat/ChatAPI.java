c8b819296536c739c43f58d1198a6e6ed0ad5478
wuhen <jianfengwuhen@163.com>
2020-01-29 20:21:52 +0800

    增加会话存档同意查询接口

--- src/main/java/com/work/wx/controller/api/chat/ChatAPI.java
+++ src/main/java/com/work/wx/controller/api/chat/ChatAPI.java
@@ -6,6 +6,12 @@
 
 package com.work.wx.controller.api.chat;
 
+import com.google.gson.JsonObject;
+import com.work.wx.config.RequestUtil;
+import com.work.wx.controller.api.token.ExternalContactAccessToken;
+import com.work.wx.controller.api.token.MsgAuditAccessToken;
+import com.work.wx.server.ChatServer;
+import com.work.wx.server.TokenServer;
 import com.work.wx.task.BackUp;
 import com.work.wx.tips.SuccessTip;
 import com.work.wx.tips.Tip;
@@ -13,10 +19,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,13 +30,49 @@ import java.util.List;
 public class ChatAPI {
     private final static Logger logger = LoggerFactory.getLogger(ChatAPI.class);
 
+    private TokenServer tokenServer;
+    private ChatServer chatServer;
 
-//    private BackUpServer backUpServer;
-//
-//    @Autowired()
-//    public void setBackUpServer(BackUpServer backUpServer) {
-//        this.backUpServer = backUpServer;
-//    }
+    @Autowired()
+    public void setChatServer(ChatServer chatServer) {
+        this.chatServer = chatServer;
+    }
+
+    public void setTokenServer(TokenServer tokenServer) {
+        this.tokenServer = tokenServer;
+    }
+
+    private String getToken() {
+        return new MsgAuditAccessToken().getMSGAUDITAccessToken(tokenServer);
+    }
+
+
+    @ApiOperation("单聊获取会话同意情况")
+    @ResponseBody
+    @RequestMapping(value = "/checkSingleAgree" ,method = RequestMethod.POST)
+    public Tip checkSingleAgree(@RequestBody String info){
+        String BASE_ADDRESS = "https://qyapi.weixin.qq.com/cgi-bin/msgaudit/check_single_agree";
+//        String json = "{\n" +
+//                "\"info\":[\n" +
+//                "{\"userid\":\"XuJinSheng\",\"exteranalopenid\":\"wmeDKaCQAAGd9oGiQWxVsAKwV2HxNAAA\"},{\"userid\":\"XuJinSheng\",\"exteranalopenid\":\"wmeDKaCQAAIQ_p7ACn_jpLVBJSGocAAA\"},\n" +
+//                "{\"userid\":\"XuJinSheng\",\"exteranalopenid\":\"wmeDKaCQAAPE_p7ABnxkpLBBJSGocAAA\"}\n" +
+//                "]\n" +
+//                "}\n";
+        return new RequestUtil().requestJsonPostDone(BASE_ADDRESS, getToken(), info);
+    }
+
+
+
+    @ApiOperation("群聊获取会话同意情况")
+    @ResponseBody
+    @RequestMapping(value = "/checkRoomAgree",method = RequestMethod.POST)
+    public Tip checkRoomAgree(@RequestParam("roomid") String roomid){
+        String BASE_ADDRESS = "https://qyapi.weixin.qq.com/cgi-bin/msgaudit/check_room_agree";
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("roomid",roomid);
+        return new RequestUtil().requestJsonPostDone(BASE_ADDRESS, getToken(), jsonObject.toString());
+    }
+
 
 
 //    @ApiOperation("获取加密归档记录")
